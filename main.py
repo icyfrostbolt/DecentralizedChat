c1b43cd961d6034f8c1aa625a099f1e64f16da4c
Ryan <ryclowe@gmail.com>
2023-05-11 10:57:27 -0700

    Multi-Channel Chat

--- main.py
+++ main.py
@@ -24,16 +24,18 @@ async def on_startup():
 )
 async def say(ctx: SlashContext, text: str):
     data = json_methods.open_file()
-    if (not str(ctx.author.id) in data["players"]):
+    if not str(ctx.author.id) in data["players"]:
         embed = interactions.Embed(
         description="You do not have a user profile! Use /create_profile to make one!",
         color=0x7CB7D3)
         await ctx.send(embeds=embed)
-    else:   
+    else:
+        channel = bot.get_channel(data["chat"]["main"]["channel"])
         embed = interactions.Embed(
         description=text,
         color=0x7CB7D3)
         embed.set_author(name=data["players"][str(ctx.author.id)]["name"], icon_url=data["players"][str(ctx.author.id)]["image"])
+        await channel.send(embeds=embed)
         await ctx.send(embeds=embed)
 
 
@@ -53,12 +55,18 @@ async def say(ctx: SlashContext, text: str):
 )
 async def profile(ctx: SlashContext, name: str, avi):
     data = json_methods.open_file()
-    data["players"][ctx.author.id] = {
-        "name":name,
-        "image":avi.url
-        }
-    json_methods.update_file(data)
-    await ctx.send("Done!")
+    if not str(ctx.author.id) in data["players"]:
+        data["players"][ctx.author.id] = {
+            "name":name,
+            "image":avi.url
+            }
+        json_methods.update_file(data)
+        await ctx.send("Done!")
+    else:  
+        embed = interactions.Embed(
+        description="You already have a user profile!",
+        color=0x7CB7D3)
+        await ctx.send(embeds=embed)
 
 
 @slash_command(name="change_name", 
@@ -71,7 +79,7 @@ async def profile(ctx: SlashContext, name: str, avi):
 )
 async def name_change(ctx: SlashContext, name: str):
     data = json_methods.open_file()
-    if (not str(ctx.author.id) in data["players"]):
+    if not str(ctx.author.id) in data["players"]:
         embed = interactions.Embed(
         description="You do not have a user profile! Use /create_profile to make one!",
         color=0x7CB7D3)
@@ -92,13 +100,35 @@ async def name_change(ctx: SlashContext, name: str):
 )
 async def image_change(ctx: SlashContext, avi):
     data = json_methods.open_file()
-    if (not str(ctx.author.id) in data["players"]):
+    if not str(ctx.author.id) in data["players"]:
         embed = interactions.Embed(
         description=f"You do not have a user profile! Use /create_profile to make one!",
         color=0x7CB7D3)
         await ctx.send(embeds=embed)
     else:  
         data["players"][str(ctx.author.id)]["image"] = avi
+        json_methods.update_file(data)
+        await ctx.send("Done!")
+
+@slash_command(name="assign_channel", 
+               description="Assign this channel to speak to be able to speak in!")
+@slash_option(
+    name="channel_name",
+    description="This is the name you will give your channel when calling it!",
+    required=True,
+    opt_type=OptionType.STRING
+)
+async def channel_assign(ctx: SlashContext, channel_name: str):
+    data = json_methods.open_file()
+    if channel_name in data["chat"]:
+        embed = interactions.Embed(
+        description="This channel has already been assigned!",
+        color=0x7CB7D3)
+        await ctx.send(embeds=embed)
+    else:  
+        data["chat"][channel_name] = {
+            "channel":ctx.channel.id,
+            }
         json_methods.update_file(data)
         await ctx.send("Done!")
 
